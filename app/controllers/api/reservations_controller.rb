a309a6ca4eef038873de5d60e31d7bc3841229db
Mohamed Badr <83557149+mbdesigns1989@users.noreply.github.com>
2023-07-14 12:56:56 +0300

    Update reservations_controller.rb

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -18,7 +18,47 @@ class Api::ReservationsController < ApplicationController
                status: :unprocessable_entity
       end
     end
-  
+  def show
+    @reservation = Reservation.find(params[:id])
+    if @reservation.present?
+        render json: { status: 'Success', message: 'Loaded Reservation', data: @reservation }, status: :ok
+      else
+        render json: { status: 'Not Found', message: 'Reservations not found', data: @reservation.errors },
+               status: :unprocessable_entity
+      end
+    end
+    def create
+    @reservation = current_user.Reservation.new(reservation_params)
+    if @reservation.present?
+        render json: { status: 'Success', message: 'Loaded Reservation', data: @reservation }, status: :ok
+      else
+        render json: { status: 'Not Found', message: 'Reservations not found', data: @reservation.errors },
+               status: :unprocessable_entity
+      end
+    end
+    def update
+        @reservation = Reservation.find(params[:id])
+        if @reservation.empty?
+            render json: { status: 'Not Found', message: 'Reservations not found', data: @reservation.errors },
+                   status: :unprocessable_entity
+        else @reservation.update(reservation_params)
+                   render json: { status: 'Success', message: 'Loaded Reservation', data: @reservation }, status: :ok
+          end
+    end
+    def destroy
+        @reservation = Reservation.find(params[:id])
+        if @reservation.empty?
+            render json: { status: 'Not Found', message: 'Reservations not found', data: @reservation.errors },
+                   status: :unprocessable_entity
+        else @reservation.destroy
+                   render json: { status: 'Success', message: 'Deleted Reservation'}, status: :ok
+          end
+    end
+    private
+    def reservation_params
+        params.require(:reservation).permit(:date, :city, :user_id, :product_id)
+    end
+      
     
   end
   
