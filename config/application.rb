ccd09bae8706d7424d60281435ef98c36a8ef2ae
Caleb Nwaizu <calebchris000@gmail.com>
2023-07-17 15:32:58 +0100

    FIx cors issue

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module BookAppointmentBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000' # Add other domains as needed
+        resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+    
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: "Mercedes Benz", image: 'url.img', description: "A describe", model: 'd3', price: 4000, mileage: 200)
