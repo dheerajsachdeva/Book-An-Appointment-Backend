ab56ddc9b92eab0a526c27a11332aacf43f9e33c
Mohamed Badr <83557149+mbdesigns1989@users.noreply.github.com>
2023-07-27 01:56:46 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-    has_many :reservations, dependent: :destroy
-    has_many :products, through: :reservations, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_many :products, through: :reservations, dependent: :destroy
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
   def jwt_payload
